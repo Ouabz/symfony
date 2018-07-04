2fe2b2ebe2f9d4774937f48b9f153f4394547624
unknown <xloadingfx@gmail.com>
2018-07-05 01:32:24 +0200

    first commit

---
+++ src/Controller/BlogController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class BlogController extends Controller
+{
+    /**
+     * @Route("/blog", name="blog")
+     */
+    public function index()
+    {
+        return $this->render('blog/index.html.twig', [
+            'controller_name' => 'BlogController',
+        ]);
+    }
+    /**
+     * @Route("/", name="home")
+     */
+    public function home(){
+        return $this->render('blog/home.html.twig', [
+            'title' => "Bienvenue ici les amies",
+            'age' => 31
+        ]);
+    }
+}
